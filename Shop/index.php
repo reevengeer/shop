0ced9afb2aeb7416e6acdaad1cd83be033084a04
reevengeer <marc.rolland@condorcet.be>
2016-10-04 11:42:44 +0200

    modification page d'accueil

--- Shop/index.php
+++ Shop/index.php
@@ -10,9 +10,19 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <h1>texte ici </h1>
-        <?php
-        // put your code here
-        ?>
+        <h2 id="titre page">  Entrées et sorties des marchandisesk</h2>
+        <div id="image accueil">
+            <section>
+            <h2> Achats des produits de décoration</h2>
+            <p>Achat de 1 276 pièces au prix moyen de 17,  35 euros</p> 
+            <p> vente de 834 pièces au prix moyen de 38,  13 euros</p> 
+            </section>
+            <section>
+                <h2> ventes des produits d'ameublement</h2>
+                <p>Achat de 816 pièces au prix moyen de 83,  45 euros</p>
+                <p>Vente de 609 pièces au prix moyen de 138,71 euros</p>
+            </section> 
+            <h2> Contact Responsable des stocks </h2>
+            <p>Julie te extension 18</p> </div>
     </body>
 </html>
